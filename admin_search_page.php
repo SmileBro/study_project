a65f0177213af563a41cef8970ef4ef5b3f6bbe1
perfectfury <kudakaev.123@gmail.com>
2023-10-10 20:58:21 +0300

    выделил форму изменения книги в отдельный файл

--- admin_search_page.php
+++ admin_search_page.php
@@ -31,14 +31,9 @@ if (isset($_POST['update_book'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>search page</title>
-
-    <!-- font awesome cdn link  -->
     <link rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-
-    <!-- custom css file link  -->
     <link rel="stylesheet" href="css/admin_style.css">
-
 </head>
 <body>
 
@@ -48,18 +43,14 @@ if (isset($_POST['update_book'])) {
     <h3>страница поиска</h3>
     <p><a href="admin_page.php">главная</a> / поиск </p>
 </div>
-
 <section class="search-form">
     <form action="" method="post">
         <input type="text" name="search" placeholder="начать поиск..."
                class="box">
         <input type="submit" name="submit" value="поиск" class="btn">
-
     </form>
 </section>
-
 <section class="books" style="padding-top: 0;">
-
     <div class="box-container">
         <?php
         if (isset($_POST['submit'])) {
@@ -96,59 +87,9 @@ if (isset($_POST['update_book'])) {
         }
         ?>
     </div>
-
-
 </section>
 
-
-<section class="edit-form">
-    <?php
-    if (isset($_GET['update'])) {
-        $update_id = $_GET['update'];
-        $update_query = mysqli_query($conn,
-            "SELECT * FROM `books` WHERE BOOK_ID = '$update_id'") or die('query failed');
-        if (mysqli_num_rows($update_query) > 0) {
-            while ($fetch_update = mysqli_fetch_assoc($update_query)) {
-                ?>
-                <form action="" method="post" enctype="multipart/form-data">
-                    <input type="hidden" name="upd_book_id"
-                           value="<?= $fetch_update['BOOK_ID'] ?>">
-                    <input type="hidden" name="update_old_image"
-                           value="<?= $fetch_update['BOOK_IMG'] ?>">
-                    <img class="book_img"
-                         src="uploaded_img/<?= $fetch_update['BOOK_IMG'] ?>"
-                         alt="">
-                    <input type="text" name="upd_book_name"
-                           value="<?= $fetch_update['BOOK_NAME'] ?>" class="box"
-                           required placeholder="Введите новое название">
-                    <input type="number" name="upd_book_amount"
-                           value="<?= $fetch_update['BOOK_AMOUNT'] ?>" min="0"
-                           class="box" required
-                           placeholder="Введите новое количество">
-                    <input type="file" class="box" name="update_image"
-                           accept="image/jpg, image/jpeg, image/png">
-
-                    <input type="submit" value="Изменить" name="update_book"
-                           class="btn">
-                    <p></p>
-
-                    <a href="admin_search_page.php" id="close-update"
-                       class="option-btn">Отменить</a>
-                </form>
-                <?php
-            }
-        }
-    }
-    else {
-        echo '<script>document.querySelector(".edit-form").style.display = "none";</script>';
-    }
-    ?>
-
-</section>
-
-<!-- custom admin js file link  -->
+<?php include 'book_edit_form.php' ?>
 <script src="js/admin_script.js"></script>
-
-
 </body>
 </html>
