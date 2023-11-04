ea97762cec2458ebc43301b7c563a304fd04e14e
perfectfury <kudakaev.123@gmail.com>
2023-11-04 11:09:08 +0300

    кнопка навигации активна на текущей странице

--- js/script.js
+++ js/script.js
@@ -1,31 +1,39 @@
 let userBox = document.querySelector('.header .header-2 .user-box');
 
 document.querySelector('#user-btn').onclick = () => {
-	userBox.classList.toggle('active');
-	navbar.classList.remove('active');
-}
+    userBox.classList.toggle('active');
+    navbar.classList.remove('active');
+};
 
 let navbar = document.querySelector('.header .header-2 .navbar');
 
 document.querySelector('#menu-btn').onclick = () => {
-	navbar.classList.toggle('active');
-	userBox.classList.remove('active');
-}
+    navbar.classList.toggle('active');
+    userBox.classList.remove('active');
+};
 
 window.onscroll = () => {
-	userBox.classList.remove('active');
-	navbar.classList.remove('active');
+    userBox.classList.remove('active');
+    navbar.classList.remove('active');
 
-	if (window.scrollY > 60) {
-		document.querySelector('.header .header-2').classList.add('active');
-	} else {
-		document.querySelector('.header .header-2').classList.remove('active');
-	}
-}
+    if (window.scrollY > 70) {
+        document.querySelector('.header .header-2').classList.add('active');
+    } else {
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    let navLinks = document.querySelectorAll('.navbar a');
+    navLinks.forEach(function(link) {
+        if (link.href === window.location.href) {
+            link.classList.add('active');
+        }
+    });
+});
 
 document.querySelector('#close-update').onclick = () => {
-	document.querySelector('.edit-form').style.display = 'none';
-	window.location = window.location.href.split('?')[0];
-}
-
+    document.querySelector('.edit-form').style.display = 'none';
+    window.location = window.location.href.split('?')[0];
+};
 
